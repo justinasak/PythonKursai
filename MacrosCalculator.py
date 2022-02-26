842525cb7ced00493f36c34d1a9368b87facf49a
justinasak <justina.sakalyt@gmail.com>
2022-02-26 23:57:52 +0200

    Entry validation

--- MacrosCalculator.py
+++ MacrosCalculator.py
@@ -1,13 +1,12 @@
 class MacrosCalculator:
-    def __init__(self, weight, height, age, gender, alvl):
+    def __init__(self, weight, height, age, gender, alvl, goal):
         self.weight = weight
         self.height = height
         self.age = age
         self.gender = gender
         self.alvl = alvl
+        self.goal = goal
+
 
     def calculatebmr(self):
-        pass
-
-
-
+        return (10 * self.weight + 6.25 * self.height - 5 * self.age + self.gender) * self.alvl + self.goal
--- main.py
+++ main.py
@@ -1,40 +1,67 @@
 from tkinter import *
+import MacrosCalculator as mc
+
+def validate_entry(input):
+    if input == "":
+        return True
+    try:
+        input1 = float(input)
+    except ValueError:
+        return False
+    return 0 <= input1 <= 100
+
+def check_weight(input):
+    if input < 15:
+        result = False
+    else:
+        result = True
+    return result
+
+def calculate():
+    weight = float(entry1.get())
+    height = float(entry2.get())
+    age = float(entry3.get())
+    gender = float(value1.get())
+    alvl = float(value2.get())
+    goal = float(value3.get())
+    calculation = mc.MacrosCalculator(weight, height, age, gender, alvl, goal)
+    result = StringVar(window, calculation.calculatebmr())
 
 window = Tk()
-window.geometry("500x600")
+window.title("Macros Calculator")
+window.geometry("500x530")
 
-value1 = StringVar(window, "1")
-value2 = StringVar(window, "3")
-value3 = StringVar(window, "2")
+value1 = StringVar(window, 5)
+value2 = StringVar(window, 1.2)
+value3 = StringVar(window, 0)
+result = ""
 
-values1 = {"Female" : "1", "Male" : "2"}
+values1 = {"Female" : -161, "Male" : 5}
 
 values2 = {
-            "Sedentary (little or no exercise)" : "2",
-            "Lightly active (light exercise/sports 1-3 days/week)" : "3",
-            "Moderately active (moderate exercise/sports 3-5 days/week)" : "4",
-            "Very active (hard exercise/sports 6-7 days a week)" : "5",
-            "If you are extra active (very hard exercise/sports & a physical job)" : "6"
+            "Sedentary (little or no exercise)" : 1.2,
+            "Lightly active (light exercise/sports 1-3 days/week)" : 1.375,
+            "Moderately active (moderate exercise/sports 3-5 days/week)" : 1.55,
+            "Very active (hard exercise/sports 6-7 days a week)" : 1.725,
+            "If you are extra active (very hard exercise/sports & a physical job)" : 1.9
           }
 
-values3 = {"Lose weight" : "1",
-           "Maintain the same weight" : "2",
-           "Gain weight" : "3"}
+values3 = {"Lose weight" : -500,
+           "Maintain the same weight" : 0,
+           "Gain weight" : 500}
 
-
-intro = Label(window, text="Macros calculator")
-
+vcmd = (window.register(validate_entry), "%P")
 label1 = Label(window, text = "Insert your weight (kg):")
-entry1 = Entry(window)
+entry1 = Entry(window, validate = "key", validatecommand=vcmd)
 label2 = Label(window, text = "Insert your height (cm):")
-entry2 = Entry(window)
+entry2 = Entry(window, validate = "key", validatecommand=vcmd)
 label3 = Label(window, text = "Insert your age:")
-entry3 = Entry(window)
+entry3 = Entry(window, validate = "key", validatecommand=vcmd)
 label4 = Label(window, text = "Select your gender: ")
 label5 = Label(window, text = "Select your activity level: ")
 label6 = Label(window, text = "Select your goal: ")
+button1 = Button(window, text = "Calculate", command=check_weight)
 
-intro.pack()
 label1.pack()
 entry1.pack()
 label2.pack()
@@ -55,5 +82,7 @@ label6.pack()
 
 for (text, value) in values3.items():
     Radiobutton(window, text = text, variable = value3, value = value).pack()
+
+button1.pack()
 
 window.mainloop()
